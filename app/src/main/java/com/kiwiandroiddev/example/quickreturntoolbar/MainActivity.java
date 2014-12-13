a8bdfd358080b40a5e98e8a94667cd6dc9e1476f
Matt <kiwiandroiddev@gmail.com>
2014-12-14 11:26:23 +1300

    building

--- app/src/main/java/com/kiwiandroiddev/example/quickreturntoolbar/MainActivity.java
+++ app/src/main/java/com/kiwiandroiddev/example/quickreturntoolbar/MainActivity.java
@@ -1,39 +1,174 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.kiwiandroiddev.example.quickreturntoolbar;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
+/**
+ * Demo activity for a quick returning toolbar.
+ *
+ * Based on Roman Nurik's Quick Return pattern implementation which can be found here
+ * (scrolltricks sub-project):
+ * https://code.google.com/p/romannurik-code/
+ */
+public class MainActivity extends FragmentActivity implements ObservableScrollView.Callbacks {
+    private static final int STATE_ONSCREEN = 0;
+    private static final int STATE_OFFSCREEN = 1;
+    private static final int STATE_RETURNING = 2;
 
-public class MainActivity extends ActionBarActivity {
+    private View mQuickReturnView;
+    private View mPlaceholderView;
+    private ObservableScrollView mObservableScrollView;
+    private ScrollSettleHandler mScrollSettleHandler = new ScrollSettleHandler();
+    private int mMinRawY = 0;
+    private int mState = STATE_ONSCREEN;
+    private int mQuickReturnHeight;
+    private int mMaxScrollY;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        toolbar.setTitle("App Title");
+        toolbar.setSubtitle("Subtitle");
+
+        mObservableScrollView = (ObservableScrollView) findViewById(R.id.scroll_view);
+        mObservableScrollView.setCallbacks(this);
+
+        mQuickReturnView = toolbar;
+        mPlaceholderView = findViewById(R.id.placeholder);
+
+        mObservableScrollView.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        onScrollChanged(mObservableScrollView.getScrollY());
+                        mMaxScrollY = mObservableScrollView.computeVerticalScrollRange()
+                                - mObservableScrollView.getHeight();
+                        mQuickReturnHeight = mQuickReturnView.getHeight();
+                    }
+                });
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onScrollChanged(int scrollY) {
+        scrollY = Math.min(mMaxScrollY, scrollY);
+
+        mScrollSettleHandler.onScroll(scrollY);
+
+        int rawY = mPlaceholderView.getTop() - scrollY;
+        int translationY = 0;
+
+        switch (mState) {
+            case STATE_OFFSCREEN:
+                if (rawY <= mMinRawY) {
+                    mMinRawY = rawY;
+                } else {
+                    mState = STATE_RETURNING;
+                }
+                translationY = rawY;
+                break;
+
+            case STATE_ONSCREEN:
+                if (rawY < -mQuickReturnHeight) {
+                    mState = STATE_OFFSCREEN;
+                    mMinRawY = rawY;
+                }
+                translationY = rawY;
+                break;
+
+            case STATE_RETURNING:
+                translationY = (rawY - mMinRawY) - mQuickReturnHeight;
+                if (translationY > 0) {
+                    translationY = 0;
+                    mMinRawY = rawY - mQuickReturnHeight;
+                }
+
+                if (rawY > 0) {
+                    mState = STATE_ONSCREEN;
+                    translationY = rawY;
+                }
+
+                if (translationY < -mQuickReturnHeight) {
+                    mState = STATE_OFFSCREEN;
+                    mMinRawY = rawY;
+                }
+                break;
+        }
+        mQuickReturnView.animate().cancel();
+        mQuickReturnView.setTranslationY(translationY + scrollY);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onDownMotionEvent() {
+        mScrollSettleHandler.setSettleEnabled(false);
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    @Override
+    public void onUpOrCancelMotionEvent() {
+        mScrollSettleHandler.setSettleEnabled(true);
+        mScrollSettleHandler.onScroll(mObservableScrollView.getScrollY());
+    }
+
+    private class ScrollSettleHandler extends Handler {
+        private static final int SETTLE_DELAY_MILLIS = 100;
+
+        private int mSettledScrollY = Integer.MIN_VALUE;
+        private boolean mSettleEnabled;
+
+        public void onScroll(int scrollY) {
+            if (mSettledScrollY != scrollY) {
+                // Clear any pending messages and post delayed
+                removeMessages(0);
+                sendEmptyMessageDelayed(0, SETTLE_DELAY_MILLIS);
+                mSettledScrollY = scrollY;
+            }
         }
 
-        return super.onOptionsItemSelected(item);
+        public void setSettleEnabled(boolean settleEnabled) {
+            mSettleEnabled = settleEnabled;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            // Handle the scroll settling.
+            if (STATE_RETURNING == mState && mSettleEnabled) {
+                int mDestTranslationY;
+                if (mSettledScrollY - mQuickReturnView.getTranslationY() > mQuickReturnHeight / 2) {
+                    mState = STATE_OFFSCREEN;
+                    mDestTranslationY = Math.max(
+                            mSettledScrollY - mQuickReturnHeight,
+                            mPlaceholderView.getTop());
+                } else {
+                    mDestTranslationY = mSettledScrollY;
+                }
+
+                mMinRawY = mPlaceholderView.getTop() - mQuickReturnHeight - mDestTranslationY;
+                mQuickReturnView.animate().translationY(mDestTranslationY);
+            }
+            mSettledScrollY = Integer.MIN_VALUE; // reset
+        }
     }
 }
