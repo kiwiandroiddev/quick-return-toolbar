048f0dc9d82beeeb41c53c68f59683662c1bd558
Matt <kiwiandroiddev@gmail.com>
2015-01-01 14:14:23 +1300

    Extracted quick return logic from MainActivity to a reusable QuickReturnHandler

--- app/src/main/java/com/kiwiandroiddev/example/quickreturntoolbar/MainActivity.java
+++ app/src/main/java/com/kiwiandroiddev/example/quickreturntoolbar/MainActivity.java
@@ -31,19 +31,8 @@ import android.view.ViewTreeObserver;
  * (scrolltricks sub-project):
  * https://code.google.com/p/romannurik-code/
  */
-public class MainActivity extends FragmentActivity implements ObservableScrollView.Callbacks {
-    private static final int STATE_ONSCREEN = 0;
-    private static final int STATE_OFFSCREEN = 1;
-    private static final int STATE_RETURNING = 2;
-
-    private View mQuickReturnView;
-    private View mPlaceholderView;
-    private ObservableScrollView mObservableScrollView;
-    private ScrollSettleHandler mScrollSettleHandler = new ScrollSettleHandler();
-    private int mMinRawY = 0;
-    private int mState = STATE_ONSCREEN;
-    private int mQuickReturnHeight;
-    private int mMaxScrollY;
+public class MainActivity extends FragmentActivity {
+    private QuickReturnHandler mQuickReturnHandler;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,122 +42,14 @@ public class MainActivity extends FragmentActivity implements ObservableScrollVi
         toolbar.setTitle("App Title");
         toolbar.setSubtitle("Subtitle");
 
-        mObservableScrollView = (ObservableScrollView) findViewById(R.id.scroll_view);
-        mObservableScrollView.setCallbacks(this);
+        ObservableScrollView mObservableScrollView = (ObservableScrollView) findViewById(R.id.scroll_view);
+        View quickReturnView = toolbar;
+        View placeholderView = findViewById(R.id.placeholder);
 
-        mQuickReturnView = toolbar;
-        mPlaceholderView = findViewById(R.id.placeholder);
-
-        mObservableScrollView.getViewTreeObserver().addOnGlobalLayoutListener(
-                new ViewTreeObserver.OnGlobalLayoutListener() {
-                    @Override
-                    public void onGlobalLayout() {
-                        onScrollChanged(mObservableScrollView.getScrollY());
-                        mMaxScrollY = mObservableScrollView.computeVerticalScrollRange()
-                                - mObservableScrollView.getHeight();
-                        mQuickReturnHeight = mQuickReturnView.getHeight();
-                    }
-                });
-    }
-
-
-    @Override
-    public void onScrollChanged(int scrollY) {
-        scrollY = Math.min(mMaxScrollY, scrollY);
-
-        mScrollSettleHandler.onScroll(scrollY);
-
-        int rawY = mPlaceholderView.getTop() - scrollY;
-        int translationY = 0;
-
-        switch (mState) {
-            case STATE_OFFSCREEN:
-                if (rawY <= mMinRawY) {
-                    mMinRawY = rawY;
-                } else {
-                    mState = STATE_RETURNING;
-                }
-                translationY = rawY;
-                break;
-
-            case STATE_ONSCREEN:
-                if (rawY < -mQuickReturnHeight) {
-                    mState = STATE_OFFSCREEN;
-                    mMinRawY = rawY;
-                }
-                translationY = rawY;
-                break;
-
-            case STATE_RETURNING:
-                translationY = (rawY - mMinRawY) - mQuickReturnHeight;
-                if (translationY > 0) {
-                    translationY = 0;
-                    mMinRawY = rawY - mQuickReturnHeight;
-                }
-
-                if (rawY > 0) {
-                    mState = STATE_ONSCREEN;
-                    translationY = rawY;
-                }
-
-                if (translationY < -mQuickReturnHeight) {
-                    mState = STATE_OFFSCREEN;
-                    mMinRawY = rawY;
-                }
-                break;
-        }
-        mQuickReturnView.animate().cancel();
-        mQuickReturnView.setTranslationY(translationY + scrollY);
-    }
-
-    @Override
-    public void onDownMotionEvent() {
-        mScrollSettleHandler.setSettleEnabled(false);
-    }
-
-    @Override
-    public void onUpOrCancelMotionEvent() {
-        mScrollSettleHandler.setSettleEnabled(true);
-        mScrollSettleHandler.onScroll(mObservableScrollView.getScrollY());
-    }
-
-    private class ScrollSettleHandler extends Handler {
-        private static final int SETTLE_DELAY_MILLIS = 100;
-
-        private int mSettledScrollY = Integer.MIN_VALUE;
-        private boolean mSettleEnabled;
-
-        public void onScroll(int scrollY) {
-            if (mSettledScrollY != scrollY) {
-                // Clear any pending messages and post delayed
-                removeMessages(0);
-                sendEmptyMessageDelayed(0, SETTLE_DELAY_MILLIS);
-                mSettledScrollY = scrollY;
-            }
-        }
-
-        public void setSettleEnabled(boolean settleEnabled) {
-            mSettleEnabled = settleEnabled;
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-            // Handle the scroll settling.
-            if (STATE_RETURNING == mState && mSettleEnabled) {
-                int mDestTranslationY;
-                if (mSettledScrollY - mQuickReturnView.getTranslationY() > mQuickReturnHeight / 2) {
-                    mState = STATE_OFFSCREEN;
-                    mDestTranslationY = Math.max(
-                            mSettledScrollY - mQuickReturnHeight,
-                            mPlaceholderView.getTop());
-                } else {
-                    mDestTranslationY = mSettledScrollY;
-                }
-
-                mMinRawY = mPlaceholderView.getTop() - mQuickReturnHeight - mDestTranslationY;
-                mQuickReturnView.animate().translationY(mDestTranslationY);
-            }
-            mSettledScrollY = Integer.MIN_VALUE; // reset
-        }
+        mQuickReturnHandler = new QuickReturnHandler(
+                toolbar,
+                placeholderView,
+                mObservableScrollView
+        );
     }
 }
